2f989dab9b341c145174c5b03148caa0838f194c
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2025-01-16 13:10:55 -0500

    chore(dialog): clean up dialog tests
    - adds hasHeroImage arg
    - adds background and margin customStyles for fullscreen dialog in
    Chromatic only (so we can see the rounded corners better)

--- components/dialog/stories/dialog.test.js
+++ components/dialog/stories/dialog.test.js
@@ -4,7 +4,7 @@ import { Template } from "./template.js";
 export const DialogGroup = Variants({
 	Template: (args, context) => (
 		/*
-		 * This template forces each test case to showModal: false, and give a background 
+		 * This template forces each test case to showModal: false, and give a background
 		 * color to the dialog grid container when isChromatic() is true.
 		 * This should ensure that the Sizes within the Variants() template to display correctly.
 		 */
@@ -12,7 +12,7 @@ export const DialogGroup = Variants({
 		Template({
 			...args,
 			showModal: window.isChromatic() ? false : args.showModal,
-			// TODO: The dialog's heading arg is getting passed as the "Sizing" heading arg (instead of the 
+			// TODO: The dialog's heading arg is getting passed as the "Sizing" heading arg (instead of the
 			// TODO: word "Sizing"). We should be able to remove this arg once that no longers happens.
 			heading: window.isChromatic() ? "Lorem ipsum dolor sit amet, consectetur adipiscing elit" : args.heading,
 			customStyles: {
@@ -37,6 +37,7 @@ export const DialogGroup = Variants({
 		},
 		{
 			testHeading: "With hero/cover image",
+			hasHeroImage: true,
 			heroImageUrl: "example-card-portrait.png",
 			wrapperStyles: {
 				"background-color": "var(--spectrum-gray-50)"
@@ -44,6 +45,7 @@ export const DialogGroup = Variants({
 		},
 		{
 			testHeading: "With hero/cover image, dismissible",
+			hasHeroImage: true,
 			heroImageUrl: "example-card-portrait.png",
 			isDismissible: true,
 			hasFooter: false,
@@ -55,7 +57,18 @@ export const DialogGroup = Variants({
 });
 
 export const DialogFullscreen = Variants({
-	Template,
+	Template: (args, context) => (
+		Template({
+			...args,
+			/*
+			 * Custom styles were added to make sure the VRTs catch the rounded corners
+			 */
+			customStyles: {
+				margin: window.isChromatic() ? "16px" : undefined,
+				"background-color": window.isChromatic() ? "var(--spectrum-gray-100)" : undefined,
+			},
+		}, context)
+	),
 	withSizes: false,
 	testData: [
 		{
