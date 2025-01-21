9a83d7708693a04230bf4da0cfd2514760ca5fbc
Aziz Ramos <abdulr@adobe.com>
2025-01-21 13:33:17 -0500

    chore(ifpc): updating story changes

--- components/infieldprogresscircle/stories/infieldprogresscircle.stories.js
+++ components/infieldprogresscircle/stories/infieldprogresscircle.stories.js
@@ -2,7 +2,6 @@ import { Sizes } from "@spectrum-css/preview/decorators";
 import { disableDefaultModes } from "@spectrum-css/preview/modes";
 import { size } from "@spectrum-css/preview/types";
 import { default as ProgressCircle } from "@spectrum-css/progresscircle/stories/progresscircle.stories.js";
-// import metadata from "../metadata/metadata.json";
 import packageJson from "../package.json";
 import { InfieldProgressCircleGroup } from "./infieldprogresscircle.test.js";
 import { Template } from "./template.js";
